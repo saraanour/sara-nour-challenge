7f54eadf14e0bcb2d561b7448978f616d76a9305
saraanour <sara.nour@statsbombservices.com>
2021-11-28 21:43:56 +0200

    adding new branch

--- cypress/integration/basic.test.js
+++ cypress/integration/basic.test.js
@@ -1,6 +1,11 @@
 context('Lodgify contact page', () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+  })
   it('Should have the right title', () => {
-    cy.visit('http://localhost:8080/Contact.html');
+    cy.visit('http://localhost:8080/contact.html');
     cy.title().should('include', 'Contact');
   });
 })
